11a26b8f5a851ed547e5d4a6393bb6fa542b4a15
Patrycja Kucharska <patrycja.kucharska@lifen.fr>
2021-03-31 11:09:27 +0200

    working .env files

--- db.go
+++ db.go
@@ -3,16 +3,21 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/jmoiron/sqlx"
 )
 
-var dbConfig string = fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
-	"localhost", 5432, "patrycjakucharska", "recipes-project")
-
 var db *sqlx.DB
 
 func connectToDB() {
+	dbHost := os.Getenv("DB_HOST")
+	dbPort, _ := strconv.Atoi(os.Getenv("DB_PORT"))
+	dbUser := os.Getenv("DB_USER")
+	dbName := os.Getenv("DB_NAME")
+	var dbConfig string = fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbName)
 	db = sqlx.MustConnect("postgres", dbConfig)
 	// defer db.Close()
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/jmoiron/sqlx v1.3.1
+	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.10.0
 )
