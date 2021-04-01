c5772a42da1f04d20a313007189c14fcd2a63f6f
Patrycja Kucharska <36930878+patrycja-kucharska@users.noreply.github.com>
2021-04-01 13:23:52 +0200

    Correct import (#7)
    * changed project structure
    
    * move pq import to db package

--- database/database.go
+++ database/database.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+  
+	// pq is used as a driver (behind the courtains) by sqlx (defined in Connect statement with "postgres" keyword)
+	_ "github.com/lib/pq"
 
 	"github.com/jmoiron/sqlx"
 )
