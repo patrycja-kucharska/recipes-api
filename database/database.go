90875218e782265d509272643bc78f263810f006
Patrycja Kucharska <patrycja.kucharska@lifen.fr>
2021-04-01 16:05:37 +0200

    logger for db done

--- database/database.go
+++ database/database.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-  
+
 	// pq is used as a driver (behind the courtains) by sqlx (defined in Connect statement with "postgres" keyword)
-	_ "github.com/lib/pq"
+	// and as a driver for loggerAdapter
+	pq "github.com/lib/pq"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/rs/zerolog"
+	sqldblogger "github.com/simukti/sqldb-logger"
+	"github.com/simukti/sqldb-logger/logadapter/zerologadapter"
 )
 
 var db *sqlx.DB
@@ -21,5 +25,9 @@ func ConnectToDB() {
 	var dbConfig string = fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
 		dbHost, dbPort, dbUser, dbName)
 	db = sqlx.MustConnect("postgres", dbConfig)
+
+	loggerAdapter := zerologadapter.New(zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout}))
+	db.DB = sqldblogger.OpenDriver(dbConfig, &pq.Driver{}, loggerAdapter)
+
 	// defer db.Close()
 }
